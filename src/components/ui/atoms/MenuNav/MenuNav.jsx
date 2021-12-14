b0eac5b69bc29dcd3a7052e3d0185470b90362bc
germanrogu <german.rodriguezgu@gmail.com>
2021-12-13 21:56:01 -0500

    :bug: :lipstick:

---
+++ src/components/ui/atoms/MenuNav/MenuNav.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Menu, MenuItem } from "@mui/material";
+import { withStyles } from "@mui/styles";
+
+import { TitleMenu } from "../TitleMenu/TitleMenu";
+
+const StyledMenu = withStyles((theme) => ({
+  paper: {
+    borderRadius: " 15px",
+    border: "1px solid #1E6594",
+  },
+}))(Menu);
+
+export const MenuNav = ({ anchorEl, open, onClose, items }) => {
+  return (
+    <>
+      <StyledMenu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={onClose}
+      >
+        {/* <NavLink to="/home" style={{ textDecoration: "none" }}>
+        <MenuItem>
+          <TitleMenu children={children} variant={"h6"} />
+        </MenuItem>
+        </NavLink> */}
+
+        {items.map((item) => (
+          <MenuItem key={item} onClick={onClose}>
+            <TitleMenu textAlign="center" children={item} variant={"h6"} />
+          </MenuItem>
+        ))}
+      </StyledMenu>
+    </>
+  );
+};
---
+++ src/components/ui/molecules/MenuOption/MenuOption.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { IconButton, Tooltip } from "@mui/material";
+
+import { MenuNav } from "../../atoms/MenuNav/MenuNav";
+
+export const MenuOption = ({ icon, tooltip, items }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openAccount = Boolean(anchorEl);
+
+  /**
+   * Open main menu
+   * @param {Event} event
+   */
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Tooltip title={tooltip}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleMenu}
+          color="inherit"
+        >
+          {icon}
+        </IconButton>
+      </Tooltip>
+
+      <MenuNav
+        anchorEl={anchorEl}
+        open={openAccount}
+        onClose={handleClose}
+        items={items}
+      />
+    </>
+  );
+};
