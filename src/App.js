ffcae963933e60174e50c18285a14f8e851a6d21
germanrogu <german.rodriguezgu@gmail.com>
2021-12-14 10:18:15 -0500

    :lipstick:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import React from 'react'
+
+
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@mui/styles'
 import { NavBar } from './components/ui/organism/AppBar/NavBar'
+
+export const theme = createTheme({
+
+    typography: {
+        fontFamily: 'IBM Plex Sans',
+
+        h1: {
+            fontSize: '2.7rem',
+        },
+        h2: {
+            fontSize: '2.3rem',
+        },
+        h3: {
+            fontSize: '1.9rem',
+        },
+        h4: {
+            fontSize: '1.5rem',
+        },
+        h5: {
+            fontSize: '1.3rem',
+        },
+        h6: {
+            fontSize: '1rem',
+        },
+        subtitle1: {
+            fontSize: '1.1rem',
+        },
+        subtitle2: {
+            fontSize: '1rem',
+        },
+        body1: {
+            fontSize: '0.9rem',
+        },
+        body2: {
+            fontSize: '0.8rem',
+        },
+
+    },
+
+
+
+
+});
+
 
 export const App = () => {
     return (
-        <>
-            <NavBar/>
-        </>
+        <ThemeProvider theme={theme}>
+            <NavBar />
+        </ThemeProvider>
+
     )
 }
--- src/components/ui/organism/AppBar/NavBar.jsx
+++ src/components/ui/organism/AppBar/NavBar.jsx
@@ -1,35 +1,51 @@
-import React, { useState } from "react";
-import { AppBar, Button, Container, Toolbar } from "@mui/material";
+import React from "react";
+import { AppBar, Button, CssBaseline, Toolbar } from "@mui/material";
 import { NavLogo } from "../../atoms/NavLogo/NavLogo";
 import logo from "../../../../img/comercio-electronico.png";
 import { Box } from "@mui/system";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { MenuOption } from "../../molecules/MenuOption/MenuOption";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  toolbar: {
+    backgroundColor: "black",
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+}));
 
 export const NavBar = () => {
+  const classes = useStyles();
+
   const pages = ["Productos", "Nosotros", "Blog"];
   const settings = ["Perfil", "Configuración", "Cerrar sesión"];
 
-  const [anchorElNav, setAnchorElNav] = useState(false);
-  const [anchorElUser, setAnchorElUser] = useState(false);
+  const handleItem = () => {
+    console.log("Item");
+  };
 
   return (
-    <AppBar position="static" elevation={1}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
+    <>
+      <CssBaseline />
+
+      <AppBar position="static" className={classes.root} elevation={1}>
+        <Toolbar disableGutters className={classes.toolbar}>
           <NavLogo logo={logo} />
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <MenuOption icon={<MenuIcon />} tooltip={"Items"} items={pages} />
           </Box>
+
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => setAnchorElNav(false)}
-                sx={{ color: "white" }}
-              >
+              <Button key={page} onClick={handleItem} sx={{ color: "white" }}>
                 {page}
               </Button>
             ))}
@@ -43,7 +59,8 @@ export const NavBar = () => {
             />
           </Box>
         </Toolbar>
-      </Container>
-    </AppBar>
+      </AppBar>
+
+    </>
   );
 };
