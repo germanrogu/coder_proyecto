934528b65c0b286f4d7aae5cc0679355c95ab134
germanrogu <german.rodriguezgu@gmail.com>
2021-12-17 16:47:09 -0500

    :sparkles: :lipstick: Agregar itemCount

--- src/components/ui/molecules/ItemListContainer/ItemListContainer.jsx
+++ src/components/ui/molecules/ItemListContainer/ItemListContainer.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Typography } from '@mui/material'
+import React from "react";
+import { Typography } from "@mui/material";
+import { ItemCount } from "../ItemCount/ItemCount";
 
+export const ItemListContainer = ({ greeting }) => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <Typography>{greeting}</Typography>
 
-export const ItemListContainer = ({greeting}) => {
-    return (
-        <div>
-            <Typography>{greeting}</Typography>
-        </div>
-    )
-}
+      <ItemCount stock={15} initial={1} />
+    </div>
+  );
+};
